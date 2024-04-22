5d1759356619df301a70091b00e2b05a12f9598b
Vatan Agnihotri <vatansharma07@gmail.com>
2024-04-23 01:37:02 +0530

    added more files

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://github.com/VatanAgnihotri">
+                <i className="fa fa-github"></i>
+              </a>
+              <a href="https://www.instagram.com/vatanagnihotri/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/vatan-agnihotri/">
+                <i className="fa fa-linkedin"></i>
+              </a>
+              <a href="https://twitter.com/AgnihotriVatan">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M{" "}
+              <span className="highlighted-text">Vatan Agnihotri</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 😎",
+                    1000,
+                    "Full stack Developer 💻",
+                    1000,
+                    "Mern stack Dev 📱",
+                    1000,
+                    "Cross Platform Dev 🔴",
+                    1000,
+                    "AWS Cloud Dev 🌐",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack of building applications with front and back end operations.
+            </span>
+          </div>
+
+          <div className="profile-options">
+            <button
+              className="btn primary-btn"
+              onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            >
+              {" "}
+              Hire Me{" "}
+            </button>
+            <a href="Vatan Agnihotri.pdf" download="Vatan Agnihotri.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
